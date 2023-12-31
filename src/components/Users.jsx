cf7e333b6070a89f32e2730768394e9bf379fbbc
hasan <yasashrafi@gmail.com>
2023-12-31 18:22:32 +0330

    clean function & abortController

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,14 +6,17 @@ function Users() {
     const [error, setError] = useState(false)
     const [id, setId] = useState("")
     useEffect(() => {
+        const controller = new AbortController()
         const fetchUsers = async () => {
             try {
                 const res = await fetch(
-                    `https://jsonplaceholder.typicode.com/users/`
+                    `https://jsonplaceholder.typicode.com/photos/${id}`,
+                    { signal: controller.signal }
                 );
                 const json = await res.json();
                 setUsers(json)
-              
+                console.log(json);
+
             } catch (error) {
                 console.log(error);
                 setError(true)
@@ -21,27 +24,30 @@ function Users() {
         };
         // console.log("Use effect runs");
         fetchUsers();
-    }, [])
+        return () => {
+            controller.abort()
+         }
+    }, [id])
 
-    const searchHandler = async () => {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-        const json = await res.json();
-        console.log(json);
-    }
+    // const searchHandler = async () => {
+    //     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    //     const json = await res.json();
+    //     console.log(json);
+    // }
     return (
         <div>
-        {id>10 &&<Counter/>}
+            {/* {id>10 &&<Counter/>} */}
             <input
                 type="text"
                 placeholder="enter id"
                 value={id} onChange={e => setId(e.target.value)} />
-            <button onClick={searchHandler}>search</button>
+            {/* <button onClick={searchHandler}>search</button> */}
 
             {!users.length && !error && <h3>Loading . . .</h3>}
 
-            <ul>
+            {/* <ul>
                 {users.map((user) => <li key={user.id}>{user.name}</li>)}
-            </ul>
+            </ul> */}
 
             {error && <h3>Something went wrong</h3>}
 
