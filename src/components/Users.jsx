45683dd9ef9e102a8ab11a4c765c66ba422e122f
hasan <yasashrafi@gmail.com>
2023-12-31 12:05:20 +0330

    fetch data with async await

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,12 +3,18 @@ import { useEffect, useState } from "react";
 function Users() {
     const [users, setUsers] = useState([])
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((res) => res.json())
-            .then((json) => setUsers(json))
+        const fetchUsers = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users")
+            const json = await res.json();
+            setUsers(json)
+            //   .then((res) => res.json())
+            //     .then((json) => setUsers(json))
+        }
+        fetchUsers()
     }, [])
     return (
         <div>
+            {!users.length && <h3>Loading . . .</h3>}
             <ul>{users.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
         </div>
     );
