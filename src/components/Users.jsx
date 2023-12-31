e673233c98742178e95fb220f103f447dc119b1d
hasan <yasashrafi@gmail.com>
2023-12-31 13:28:02 +0330

    handle errors and loading

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 
 function Users() {
     const [users, setUsers] = useState([])
+    const [error, setError] = useState(false)
     useEffect(() => {
         const fetchUsers = async () => {
-            const res = await fetch("https://jsonplaceholder.typicode.com/users")
-            const json = await res.json();
-            setUsers(json)
-            //   .then((res) => res.json())
-            //     .then((json) => setUsers(json))
-        }
-        fetchUsers()
+            try {
+                const res = await fetch("https://jsonplaceholder.typicode.com/users")
+                const json = await res.json();
+                setUsers(json)
+            } catch (error) {
+                console.log(error);
+                setError(true)
+            }
+        };
+        fetchUsers();
     }, [])
     return (
         <div>
-            {!users.length && <h3>Loading . . .</h3>}
+            {!users.length &&!error&& <h3>Loading . . .</h3>}
             <ul>{users.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+        {error && <h3>Something went wrong</h3>}
         </div>
     );
 }
