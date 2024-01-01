a1fde975710fdc6c25ac21e145e0249b87d039cf
hasan <yasashrafi@gmail.com>
2024-01-01 12:25:07 +0330

    state batching

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from "react";
 
 function Photos() {
     const [isShow, setIsShow] = useState(false)
+    const [text, setText] = useState("")
+    const [count, setCount] = useState(0)
 
 
-    const isShowHandler = () => {
-        setIsShow(true);
+    const clickHandler = () => {
+        setIsShow(isShow => !isShow)
+        setText(text => "hasan")
+        setCount(count => count + 1)
     }
     return (
         <div>
             <h1>Hello World!💻</h1>
+            <p>{count}</p>
             {isShow && <h2>React Course</h2>}
-            <button onClick={() => setIsShow(isShow => !isShow)}>Toggle</button>
+            <h3>{text}</h3>
+            <button onClick={clickHandler}>Toggle</button>
         </div>
     );
 }
